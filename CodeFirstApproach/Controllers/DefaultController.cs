bd559a6e5267744bbf534894580bf5a2a3958f86
yawar2019 <yawarali17@gmail.com>
2019-10-16 00:13:00 +0530

    Add Code first Approach Files

---
+++ CodeFirstApproach/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CodeFirstApproach.Models;
+namespace CodeFirstApproach.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        EmployeeContext db = new EmployeeContext();
+        public ActionResult Index()
+        {
+            var employeedata = db.EmployeeModels.ToList();
+            return View(employeedata);
+        }
+    }
+}
